4214b655f25878ec41a64d1cdc7a5a99eab740d7
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-02 18:07:50 +0000

    gv

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function calculate() {
+  const age = parseInt(document.getElementById("age").value)
+  const day = parseInt(document.getElementById("day").value)
+
+  if ( (day == "Saturday" || day == "Sunday") || age > 12 && age < 21 ) {
+    document.getElementById("answer").innerHTML =
+      "You're get the student pricing!"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "You myst pay regular price."
+  }
+}

a2d9c433038c308226bc4df2f16ec337cb20b4bd
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-15 18:27:43 +0000

    wrtsgsg

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 // Created by: Calista.E
 // Created on: May 2024
 // This file contains the JS functions for index.html
+
 "use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
 
 function calculate() {
   const age = parseInt(document.getElementById("age").value)
